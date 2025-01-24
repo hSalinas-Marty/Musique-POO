67a8e0627d771791a430f5ad4bcb9463c409b926
matheo <mmaussant08@gmail.com>
2025-01-24 09:26:52 +0100

    marche sur linnux

--- Instruments.cpp
+++ Instruments.cpp
@@ -1,42 +1,69 @@
+#include <iostream>
+#include <unistd.h>
+#include <termios.h>
+#include <cstdlib>
 #include "Instruments.hpp"
 
-void Touch::listenForKeyPress()
-{
-        cout << "Appuyez sur une touche correspondant � une note musicale (d, r, m, f, s, l, i)." << endl;
-        cout << "Appuyez sur 'q' pour quitter." << endl;
+using namespace std;
 
-        while (true) {
-            if (_kbhit()) {
-                char key = _getch();
-
-                switch (key) {
-                case 'd':
-                    cout << "note_do" << endl;
-                    break;
-                case 'f':
-                    cout << "note_re" << endl;
-                    break;
-                case 'g':
-                    cout << "note_mi" << endl;
-                    break;
-                case 'h':
-                    cout << "note_fa" << endl;
-                    break;
-                case 'j':
-                    cout << "note_sol" << endl;
-                    break;
-                case 'k':
-                    cout << "note_la" << endl;
-                    break;
-                case 'l':
-                    cout << "note_si" << endl;
-                    break;
-                case 'q':
-                    cout << "Fin du programme." << endl;
-                    return;
-                default:
-                    cout << "Touche non reconnue." << endl;
-                }
-            }
-        }
+// Fonction pour obtenir une entrée sans appuyer sur 'Entrée'
+char getKeyPress() {
+    struct termios oldt, newt;
+    char ch;
+    
+    // Sauvegarde des paramètres du terminal
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    
+    // Désactive le mode canonique et les échos
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    
+    // Attente d'une touche
+    ch = getchar();
+    
+    // Restauration des paramètres du terminal
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    
+    return ch;
 }
+
+void Touch::listenForKeyPress() {
+    cout << "Appuyez sur une touche correspondant à une note musicale (d, r, m, f, s, l, i)." << endl;
+    cout << "Appuyez sur 'q' pour quitter." << endl;
+
+    while (true) {
+        char key = getKeyPress();
+
+        switch (key) {
+        case 'd':
+            cout << "Note: " << note_do << endl;
+            break;
+        case 'r':
+            cout << "Note: " << note_re << endl;
+            break;
+        case 'm':
+            cout << "Note: " << note_mi << endl;
+            break;
+        case 'f':
+            cout << "Note: " << note_fa << endl;
+            break;
+        case 's':
+            cout << "Note: " << note_sol << endl;
+            break;
+        case 'l':
+            cout << "Note: " << note_la << endl;
+            break;
+        case 'i':
+            cout << "Note: " << note_si << endl;
+            break;
+        case 'q':
+            cout << "Fin du programme." << endl;
+            return;
+        default:
+            cout << "Touche non reconnue." << endl;
+        }
+    }
+}
+
+
